91b91eb0de89ba15489f9835c7daf866845840ca
akshay <hiremath2akshay@gmail.com>
2020-11-03 16:28:45 +0530

    added bootstrap and jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+#create a blog models
+
+#Add the Blog app to the settings
+
+#create a migration
+
+#migrate
+
+#add to the admin
+
+class Blog(models.Model):
+    #title
+    #pub_date 
+    #body
+    #image
+
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    # def __str__(self):
+    #     return self.title
+
+    # def summary(self):
+    #     return self.body[:100]
+
+    # def pub_date_pretty(self):
+    #     return self.pub_date.strftime('%b %e %Y')
