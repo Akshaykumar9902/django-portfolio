35721f7ef7598c3857ed55480ceafd0acf195e9a
akshay <hiremath2akshay@gmail.com>
2020-11-05 16:04:28 +0530

    Blog functionallity and polish

--- blog/models.py
+++ blog/models.py
@@ -21,11 +21,11 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return self.title
 
-    # def summary(self):
-    #     return self.body[:100]
+    def summary(self):
+        return self.body[:100]
 
-    # def pub_date_pretty(self):
-    #     return self.pub_date.strftime('%b %e %Y')
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y' )
