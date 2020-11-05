e0c3ea5bc22c0bc8d5ecb376a19814442e47ca27
akshay <hiremath2akshay@gmail.com>
2020-11-05 19:18:49 +0530

    mysql

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'portfoliodb',
-        'USER':'postgres',
-        'PASSWORD':'fcml@123',
+        'USER':'root',
+        'PASSWORD':'',
         'HOST':'localhost',
-        'PORT':'5432',
+        'PORT':'3306',
     }
 }
 
